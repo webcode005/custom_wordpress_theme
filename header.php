8af73bcf0cf67b40826153520145d1782079f74f
Basant Mallick <87462488+webcode005@users.noreply.github.com>
2025-08-13 11:42:35 +0530

    Update header.php

---
+++ header.php
@@ -0,0 +1,5 @@
+Take everything from your template’s <head> section (title, meta, links) and put it here.
+Replace the <title> tag with:
+
+<?php wp_head(); ?>
+and ensure the <body> starts here too.
